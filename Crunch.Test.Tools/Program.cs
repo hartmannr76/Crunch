7dcd616a2f84214e753bca7f87ea2243be3701e4
hartmannr76 <hartmannr76@gmail.com>
2016-10-17 05:31:08 -0400

    Add new project file to tests project

---
+++ Crunch.Test.Tools/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Crunch.Test.Tools
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var url = Environment.GetEnvironmentVariable("RUNNING_URL");
+            var port = Environment.GetEnvironmentVariable("PORT");
+            Console.Out.WriteLine(string.Format("Running url: {0} ", url));
+            Console.Out.WriteLine(string.Format("Port: {0} ", port));
+            var hostBuilder = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseUrls(url+":"+port)
+                .UseStartup<Startup>();
+            
+            var host = hostBuilder.Build();    
+            host.Run();
+        }
+    }
+}
