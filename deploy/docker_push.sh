969e49c6c0e606e3124546f17f75d7ca8f8e8444
hartmannr76 <hartmannr76@gmail.com>
2016-11-24 22:02:52 -0500

    Lets see if travis can push

--- deploy/docker_push.sh
+++ deploy/docker_push.sh
@@ -1,18 +1,18 @@
-if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  if [ "$TRAVIS_BRANCH" == "master" ]; then
+# if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  # if [ "$TRAVIS_BRANCH" == "master" ]; then
     pip install --user awscli
     export PATH=$PATH:$HOME/.local/bin
     eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
     
     # Build and push
-    docker build -t $IMAGE_NAME .
+    # docker build -t $IMAGE_NAME .
     echo "Pushing $IMAGE_NAME:latest"
     docker tag $IMAGE_NAME:latest "$REMOTE_IMAGE_URL:latest"
     docker push "$REMOTE_IMAGE_URL:latest"
     echo "Pushed $IMAGE_NAME:latest"
-  else
-    echo "Skipping deploy because branch is not 'master'"
-  fi
-else
-  echo "Skipping deploy because it's a pull request"
-fi
+  # else
+    # echo "Skipping deploy because branch is not 'master'"
+#   fi
+# else
+#   echo "Skipping deploy because it's a pull request"
+# fi
