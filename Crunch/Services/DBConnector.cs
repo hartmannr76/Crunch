416622641389d1ef3009ea3c8cdc211433a8378e
hartmannr76 <hartmannr76@gmail.com>
2017-01-17 06:28:23 -0500

    Parse connection string custom in prod

--- Crunch/Services/DBConnector.cs
+++ Crunch/Services/DBConnector.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using EasyIoC;
 using EasyIoC.Attributes;
 using Crunch.Extensions;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Crunch.Services
 {
@@ -17,17 +18,45 @@ namespace Crunch.Services
     public class DBConnector : IDBConnector {
         private readonly ConnectionMultiplexer _redisContext;
         private readonly ILogger<DBConnector> _logger;
+        private readonly IHostingEnvironment _hostingEnvironment;
         public bool IsConnected { get; private set; }
         
         public DBConnector(
-            ILogger<DBConnector> logger) {
+            ILogger<DBConnector> logger,
+            IHostingEnvironment hostingEnvironment) {
             _logger = logger;
+            _hostingEnvironment = hostingEnvironment;
+            
+
+            var redisHost = Environment.GetEnvironmentVariable("REDIS_PORT_6379_TCP_ADDR");
 
             try {
-                var redisHost = Environment.GetEnvironmentVariable("REDIS_PORT_6379_TCP_ADDR");
+                if (_hostingEnvironment.IsProduction()) {
+                    var splitConfig = redisHost.Split(new [] { "//" }, StringSplitOptions.None);
 
-                _logger.LogInformation("Attempting to connect to: {0}".FormatWith(redisHost));
-                _redisContext = ConnectionMultiplexer.Connect(redisHost);
+                    var configParams = splitConfig[1].Split('@');
+
+                    var creds = configParams[0].Split(':');
+                    var password = creds[1];
+
+                    var connectionParams = configParams[1].Split(':');
+                    var host = connectionParams[0];
+                    var port = connectionParams[1];
+
+                    _redisContext = ConnectionMultiplexer.Connect(
+                        "{0},ssl=true,password={1},name={2}".FormatWith(
+                            connectionParams,
+                            password,
+                            "crunch_api"
+                        )
+                    );
+                }
+                else {
+                    var config = ConfigurationOptions.Parse(redisHost);
+
+                    _logger.LogInformation("Attempting to connect to: {0}".FormatWith(redisHost));
+                    _redisContext = ConnectionMultiplexer.Connect(redisHost);
+                }
 
                 IsConnected = true;
             } catch (Exception e) {
